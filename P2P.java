b3727b5603b0525deedf8e0d1e223625114011e0
RobertMasek <51871255+RobertMasek@users.noreply.github.com>
2020-03-01 16:44:34 -0600

    Multithreaded
    only done testing on my own computer, still needs file I/O and the display eventually

--- P2P.java
+++ P2P.java
@@ -1,88 +1,75 @@
 package p2p;
 
-import java.io.IOException;
 import java.net.*;
-import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Iterator;
+import java.util.Map;
 
 public class P2P {
 //	private double version;
-	DatagramSocket socket = null;
+	private DatagramSocket socket = null;
+	private Map<String, Long> test;
 	
-	public P2P() {
-//		this.version = version;
-	}
-	
-	public void createAndListenSocket() 
-    {
-        try 
-        {
-        	// get my IP address
-        	InetAddress myIP = InetAddress.getByName("localhost");
-        	
-        	// Create a socket for each IP and send data
-        	// List of IP addresses that I'm going to send to
-        	ArrayList<String> initialIPList = new ArrayList<>();
-        	initialIPList.add("150.243.200.173");
-        	initialIPList.add("150.243.200.142");
-        	// Create socket
-        	socket = new DatagramSocket(9876);
-        	// Send IP address to other machines
-        	for (int i = 0; i < initialIPList.size(); i++)
-        	{
-        		InetAddress destIP = InetAddress.getByName(initialIPList.get(i));;
-        		String sentence = myIP.toString();
-	            byte[] data = sentence.getBytes();
-	            DatagramPacket sendPacket = new DatagramPacket(data, data.length, destIP, 9876);
-	            socket.send(sendPacket);
-	            System.out.println("IP sent from " + myIP.toString());
-        	}
-           
-        	// wait to receive IP addresses from other machines
-        	
-        	
-            byte[] incomingData = new byte[1024];
-            
-            while (true) 
-            {
-            	// receive data from an IP
-                DatagramPacket incomingPacket = new DatagramPacket(incomingData, 
-                		incomingData.length);
-                socket.receive(incomingPacket);
-                String message = new String(incomingPacket.getData());
-                InetAddress otherIP = incomingPacket.getAddress();
-                int port = incomingPacket.getPort();
-                
-                System.out.println("Received IP from peer: " + message);
-                System.out.println("Other peer IP: "+ otherIP.getHostAddress());
-                System.out.println("Other peer port:"+ port);
-                
-                String reply = "Thank you for the message";
-                byte[] data = reply.getBytes();
-                
-                DatagramPacket replyPacket =
-                        new DatagramPacket(data, data.length, otherIP, port);
-//                socket.close();
-//                socket.send(replyPacket);
-                Thread.sleep(2000);
-            }
-            
-        } 
-        catch (SocketException e) 
-        {
-            e.printStackTrace();
-        } 
-        catch (IOException i) 
-        {
-            i.printStackTrace();
-        } 
-        catch (InterruptedException e) 
+	public P2P() 
+	{
+		try
+		{
+			this.socket = new DatagramSocket(9876);
+		}
+		catch (SocketException e) 
         {
             e.printStackTrace();
         }
-    }
+		this.test = new HashMap<String, Long>();
+		//initial ip addresses
+		test.put("localhost", new Long(0));
+	}
 	
-	public static void main(String args[]) {  
+	//adds a timestamp to the map, will replace existing ones
+	public synchronized void addToMap(String IP, Long val)
+	{
+		test.put(IP, val);
+	}
+	
+	//gets an iterator of all the active ip addresses
+	public synchronized Iterator<String> getIterator()
+	{
+		return test.keySet().iterator();
+	}
+	
+	//gets the timestamp associated with a key
+	public synchronized long getValue(String key)
+	{
+		return test.get(key);
+	}
+	
+	//clears the map of all timestamps
+	public synchronized void clearMap()
+	{
+		test.clear();
+	}
+	
+	//removes a timestamp from the map
+	public synchronized void removeFromMap(String IP)
+	{
+		test.remove(IP);
+	}
+	
+	public static void main(String args[]) 
+	{  
 		P2P peer = new P2P();
-        peer.createAndListenSocket();
+		Thread receiver = new Thread(new P2PReceiver(peer.socket, peer));
+		Thread sender = new Thread(new P2PSender(peer.socket, peer));
+		receiver.start();
+		sender.start();
+		try
+		{
+			receiver.join();
+			sender.join();
+		}
+		catch(InterruptedException e)
+		{
+			e.printStackTrace();
+		}
 	}
 }
