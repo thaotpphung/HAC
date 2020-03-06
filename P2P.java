52f65c1852bfe8c71ac9c541ad41848bb8969feb
ThaoPhung <34802069+thaotpphung@users.noreply.github.com>
2020-03-06 16:22:20 -0600

    update p2p

--- P2P.java
+++ P2P.java
@@ -9,63 +9,58 @@ public class P2P {
 	// private double version;
 	private DatagramSocket socket = null;
 	// Map that maps IP addresses to time stamps
-	private Map<String, Long> test;
-	
-	public P2P() 
-	{
-		try
-		{
+	private Map<String, Long> IpStampMap;
+
+	public P2P() {
+		try {
 			// create my socket
 			this.socket = new DatagramSocket(9876);
+		} catch (SocketException e) {
+			e.printStackTrace();
 		}
-		catch (SocketException e) 
-        {
-            e.printStackTrace();
-        }
-		this.test = new HashMap<String, Long>();
-		// initialize the list of all peers' IP addresses
-		test.put("localhost", new Long(0));
+		this.IpStampMap = new HashMap<String, Long>();
+		// initializes the list of all peers' IP addresses
+		// the time stamps are all initialized to 0
+
+		// Annie's IP
+		IpStampMap.put("150.243.198.135", new Long(0));
+		// Robert's IP
+		IpStampMap.put("150.243.213.204", new Long(0));
+		// Josh's IP
+//		IpStampMap.put("150.243.198.135", new Long(0));
 		
 	}
-	
-	//adds a time stamp to the map, will replace existing ones
-	public synchronized void addToMap(String IP, Long val)
-	{
-		test.put(IP, val);
+
+	// adds a time stamp to the map, will replace existing ones
+	public synchronized void addToMap(String IP, Long val) {
+		IpStampMap.put(IP, val);
 	}
-	
-	//gets an iterator of all the active ip addresses
-	public synchronized Iterator<String> getIterator()
-	{
-		return test.keySet().iterator();
+
+	// gets an iterator of all the active IP addresses
+	public synchronized Iterator<String> getIterator() {
+		return IpStampMap.keySet().iterator();
 	}
-	
-	//gets the time stamp associated with a key
-	public synchronized long getValue(String key)
-	{
-		return test.get(key);
+
+	// gets the time stamp associated with a key
+	public synchronized long getValue(String key) {
+		return IpStampMap.get(key);
 	}
-	
-	//removes a time stamp from the map
-	public synchronized void removeFromMap(String IP)
-	{
-		test.remove(IP);
+
+	// removes a time stamp from the map
+	public synchronized void removeFromMap(String IP) {
+		IpStampMap.remove(IP);
 	}
-	
-	public static void main(String args[]) 
-	{  
+
+	public static void main(String args[]) {
 		P2P peer = new P2P();
 		Thread receiver = new Thread(new P2PReceiver(peer.socket, peer));
 		Thread sender = new Thread(new P2PSender(peer.socket, peer));
 		receiver.start();
 		sender.start();
-		try
-		{
+		try {
 			receiver.join();
 			sender.join();
-		}
-		catch(InterruptedException e)
-		{
+		} catch (InterruptedException e) {
 			e.printStackTrace();
 		}
 	}
