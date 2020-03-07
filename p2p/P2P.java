89849c0a3e12f2b018bb5d57e0cd08dee3917c22
ThaoPhung <34802069+thaotpphung@users.noreply.github.com>
2020-03-07 14:48:43 -0600

    update p2p

--- p2p/P2P.java
+++ p2p/P2P.java
@@ -3,8 +3,11 @@ package p2p;
 import java.net.*;
 import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.HashSet;
 import java.util.Iterator;
+import java.util.LinkedHashSet;
 import java.util.Map;
+import java.util.Set;
 
 public class P2P {
 	// private double version;
@@ -24,33 +27,53 @@ public class P2P {
 		// the time stamps are all initialized to 0
 
 		// Annie's IP
-		peerInfoList.add(new PeerInfo("150.243.198.135", new Long(0)));
+//		peerInfoList.add(new PeerInfo("150.243.198.135", new Long(0)));
 		// Robert's IP
-		peerInfoList.add(new PeerInfo("150.243.213.204", new Long(0)));
+//		peerInfoList.add(new PeerInfo("150.243.213.204", new Long(0)));
 		// Josh's IP
-		peerInfoList.add(new PeerInfo("150.243.211.102", new Long(0)));
+//		peerInfoList.add(new PeerInfo("150.243.211.102", new Long(0)));
+		peerInfoList.add(new PeerInfo("192.168.1.112", new Long(0)));
+		peerInfoList.add(new PeerInfo("192.168.1.109", new Long(0)));
+		
 	}
 
 	// adds to active list
 	public synchronized void addToList(String ip, Long val) {
+		
+		removeFromList(ip);
+		
 		peerInfoList.add(new PeerInfo(ip, val));
+		
+		
+		System.out.println("add " + ip + " to peer, The active list:");
+		for (int i = 0; i < peerInfoList.size(); i++)
+		{
+			System.out.print(peerInfoList.get(i).getIp() + " ");
+		}
+		System.out.println();
 	}
-
-//		// removes from the active list
-//		public synchronized void removeFromList(String ip) {
-//			for (int i = 0; i< peerInfoList.size(); i++)
-//			{
-//				if (peerInfoList.get(i).getIp().equals(ip))
-//				{
-//					peerInfoList.remove(i);
-//				}
-//			}
-//		}
 
 	// removes from the active list
 	public synchronized void removeFromList(PeerInfo aPeer) {
 		peerInfoList.remove(aPeer);
+		System.out.println("remove " + aPeer.getIp() + " from peer, The active list:");
+		for (int i = 0; i < peerInfoList.size(); i++)
+		{
+			System.out.print(peerInfoList.get(i).getIp() + " ");
+		}
+		System.out.println();
 	}
+	
+	public synchronized void removeFromList(String ip) {
+		for (int i = 0; i < peerInfoList.size(); i++)
+		{
+			if (peerInfoList.get(i).getIp().equals(ip))
+			{
+				peerInfoList.remove(i);
+			}
+		}
+	}
+	
 
 	public synchronized ArrayList<PeerInfo> getList() {
 		return this.peerInfoList;
