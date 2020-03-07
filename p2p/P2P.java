12b9ac0171bcb0a02e6a5a1ce9413e8c97b8acad
ThaoPhung <34802069+thaotpphung@users.noreply.github.com>
2020-03-07 15:28:41 -0600

    annie - update p2p class

--- p2p/P2P.java
+++ p2p/P2P.java
@@ -25,23 +25,28 @@ public class P2P {
 		this.peerInfoList = new ArrayList<PeerInfo>();
 		// initializes the list of all peers' IP addresses
 		// the time stamps are all initialized to 0
-
+		
+		
+		// add the others' IP addresses to the list of active IP address here
+		
 		// Annie's IP
 //		peerInfoList.add(new PeerInfo("150.243.198.135", new Long(0)));
 		// Robert's IP
 //		peerInfoList.add(new PeerInfo("150.243.213.204", new Long(0)));
 		// Josh's IP
 //		peerInfoList.add(new PeerInfo("150.243.211.102", new Long(0)));
-		peerInfoList.add(new PeerInfo("192.168.1.112", new Long(0)));
+//		peerInfoList.add(new PeerInfo("192.168.1.112", new Long(0)));
 		peerInfoList.add(new PeerInfo("192.168.1.109", new Long(0)));
+		peerInfoList.add(new PeerInfo("150.243.16.95", new Long(0)));
 		
 	}
 
 	// adds to active list
 	public synchronized void addToList(String ip, Long val) {
-		
+		// remove duplicate
 		removeFromList(ip);
 		
+		// add to active list
 		peerInfoList.add(new PeerInfo(ip, val));
 		
 		
@@ -53,7 +58,7 @@ public class P2P {
 		System.out.println();
 	}
 
-	// removes from the active list
+	// removes from the active list by PeerInfo
 	public synchronized void removeFromList(PeerInfo aPeer) {
 		peerInfoList.remove(aPeer);
 		System.out.println("remove " + aPeer.getIp() + " from peer, The active list:");
@@ -64,6 +69,7 @@ public class P2P {
 		System.out.println();
 	}
 	
+	// removes from the active list by IP address
 	public synchronized void removeFromList(String ip) {
 		for (int i = 0; i < peerInfoList.size(); i++)
 		{
@@ -74,7 +80,7 @@ public class P2P {
 		}
 	}
 	
-
+	// return the list of active IP addresses
 	public synchronized ArrayList<PeerInfo> getList() {
 		return this.peerInfoList;
 	}
