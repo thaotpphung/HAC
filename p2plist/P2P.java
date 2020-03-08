93bbc9790c161c9e714234f5eb1f1fffb19c3a77
ThaoPhung <34802069+thaotpphung@users.noreply.github.com>
2020-03-07 23:11:12 -0600

    Update P2P.java

--- p2plist/P2P.java
+++ p2plist/P2P.java
@@ -10,10 +10,8 @@ import java.util.Map;
 import java.util.Set;
 
 public class P2P {
-	// private double version;
 	private DatagramSocket socket = null;
-	// Map that maps IP addresses to time stamps
-	private ArrayList<PeerInfo> peerInfoList;
+	private ArrayList<PeerInfo> activeList; // list of active peers
 
 	public P2P() {
 		try {
@@ -22,67 +20,37 @@ public class P2P {
 		} catch (SocketException e) {
 			e.printStackTrace();
 		}
-		this.peerInfoList = new ArrayList<PeerInfo>();
+		this.activeList = new ArrayList<PeerInfo>();
+
 		// initializes the list of all peers' IP addresses
 		// the time stamps are all initialized to 0
-		
-		
 		// add the others' IP addresses to the list of active IP address here
-		
-		// Annie's IP
-//		peerInfoList.add(new PeerInfo("150.243.198.135", new Long(0)));
-		// Robert's IP
-//		peerInfoList.add(new PeerInfo("150.243.213.204", new Long(0)));
-		// Josh's IP
-//		peerInfoList.add(new PeerInfo("150.243.211.102", new Long(0)));
-//		peerInfoList.add(new PeerInfo("192.168.1.112", new Long(0)));
-		peerInfoList.add(new PeerInfo("192.168.1.109", new Long(0)));
-		peerInfoList.add(new PeerInfo("150.243.16.36", new Long(0)));
-		
+
+		activeList.add(new PeerInfo("192.168.1.109", new Long(0), false));
+		activeList.add(new PeerInfo("150.243.16.36", new Long(0), false));
+
 	}
 
-	// adds to active list
-	public synchronized void addToList(String ip, Long val) {
-		// remove duplicate
-		removeFromList(ip);
-		
-		// add to active list
-		peerInfoList.add(new PeerInfo(ip, val));
-		
-		
-		System.out.println("add " + ip + " to peer, The active list:");
-		for (int i = 0; i < peerInfoList.size(); i++)
+	// a peer is active, change status to on, update timestamp
+	public synchronized void turnOn(String ip, Long val) {
+		for (int i = 0; i < activeList.size(); i++)
 		{
-			System.out.print(peerInfoList.get(i).getIp() + " ");
-		}
-		System.out.println();
-	}
-
-	// removes from the active list by PeerInfo
-	public synchronized void removeFromList(PeerInfo aPeer) {
-		peerInfoList.remove(aPeer);
-		System.out.println("remove " + aPeer.getIp() + " from peer, The active list:");
-		for (int i = 0; i < peerInfoList.size(); i++)
-		{
-			System.out.print(peerInfoList.get(i).getIp() + " ");
-		}
-		System.out.println();
-	}
-	
-	// removes from the active list by IP address
-	public synchronized void removeFromList(String ip) {
-		for (int i = 0; i < peerInfoList.size(); i++)
-		{
-			if (peerInfoList.get(i).getIp().equals(ip))
+			if(activeList.get(i).getIp().equals(ip))
 			{
-				peerInfoList.remove(i);
+				activeList.get(i).setStatus(true);
+				activeList.get(i).setlastReceived(val);
 			}
 		}
 	}
-	
+
+	// a peer is offline, change status to off
+	public synchronized void turnOff(PeerInfo aPeer) {
+		aPeer.setStatus(false);
+	}
+
 	// return the list of active IP addresses
 	public synchronized ArrayList<PeerInfo> getList() {
-		return this.peerInfoList;
+		return this.activeList;
 	}
 
 	public static void main(String args[]) {
