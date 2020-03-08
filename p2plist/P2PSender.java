7b8f52564692c20449924c6ccfe79868914f0f6b
ThaoPhung <34802069+thaotpphung@users.noreply.github.com>
2020-03-07 23:14:52 -0600

    Update P2PSender.java

--- p2plist/P2PSender.java
+++ p2plist/P2PSender.java
@@ -5,45 +5,92 @@ import java.net.DatagramPacket;
 import java.net.DatagramSocket;
 import java.net.InetAddress;
 import java.net.SocketException;
+import java.util.ArrayList;
+import java.util.Random;
 
-public class P2PReceiver implements Runnable
+public class P2PSender implements Runnable
 {
 	private P2P peer;
 	private DatagramSocket socket;
+	private Random timer;
 	
-	public P2PReceiver(DatagramSocket socket, P2P peer)
+	public P2PSender(DatagramSocket socket, P2P peer)
 	{
 		this.peer = peer;
 		this.socket = socket;
+		this.timer = new Random();
 	}
 	
 	public void run()
 	{
 		try
 		{
-			byte[] incomingData = new byte[1024];
-
-            		while(true) 
-            		{
-            			// receive data from another peer
-                		DatagramPacket incomingPacket = new DatagramPacket(incomingData, incomingData.length);
-                		socket.receive(incomingPacket);
-                
-                		// get peer's IP address and output it
-                		InetAddress otherIP = incomingPacket.getAddress();
-                		System.out.println("IP address received from " + otherIP.toString().substring(1) + "\n");
-				                
-                		//update the map with the correct time stamp
-                		peer.turnOn(otherIP.toString().substring(1), System.currentTimeMillis());
-            		}
+			// get local IP address to send to other peers
+			InetAddress myIP = InetAddress.getByName("localhost");
+			
+			
+			// initial setup: first, send my IP address to other peers 
+			
+			// get the active IP list
+			ArrayList<PeerInfo> ipList = peer.getList();
+			
+			// iterate through active IP addresses
+			for (int i = 0; i < ipList.size(); i++)
+			{
+				PeerInfo currentIP = ipList.get(i);
+				// send my IP address to the current peer
+				InetAddress destIP = InetAddress.getByName(currentIP.getIp());
+				String sentence = myIP.toString();
+				byte[] data = sentence.getBytes();
+				DatagramPacket sendPacket = new DatagramPacket(data, data.length, destIP, 9876);
+				socket.send(sendPacket);
+				System.out.println("IP sent to " + currentIP.getIp()+ "\n");
+			}
+			
+					
+			// sleep a random amount of time from 0-30 seconds 
+			Thread.sleep(timer.nextInt(30000));
+			
+			// now, send my IP address to any peer who replies
+			while(true)
+			{
+				// get a new iterator though IP addresses
+				ipList = peer.getList();
+				
+				for (PeerInfo aPeer : ipList)
+				{
+					if (aPeer.getStatus() == true)
+					{
+						if(aPeer.getlastReceived() <= System.currentTimeMillis() - 30000)//if peer has not been active in the last 30 seconds
+						{
+							System.out.print("time out: ");
+							peer.turnOff(aPeer);
+						}
+						else//if peer is active
+						{
+							InetAddress destIP = InetAddress.getByName(aPeer.getIp());
+							String sentence = myIP.toString();
+							byte[] data = sentence.getBytes();
+							DatagramPacket sendPacket = new DatagramPacket(data, data.length, destIP, 9876);
+							socket.send(sendPacket);
+							System.out.println("IP sent to " + aPeer.getIp() + "\n");
+						}
+					}	
+				}
+				Thread.sleep(timer.nextInt(30000));
+			}
 		}
 		catch (SocketException e) 
         {
-			e.printStackTrace();
+            e.printStackTrace();
         } 
-		catch (IOException i) 
+        catch (IOException i) 
         {
             i.printStackTrace();
         } 
+        catch (InterruptedException e) 
+        {
+            e.printStackTrace();
+        }
 	}
 }
