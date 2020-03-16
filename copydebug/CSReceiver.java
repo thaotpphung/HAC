4a4bebb9fb574c1fc694f685cf6924fedc5e6c91
ThaoPhung <34802069+thaotpphung@users.noreply.github.com>
2020-03-16 17:17:53 -0500

    de

--- copydebug/CSReceiver.java
+++ copydebug/CSReceiver.java
@@ -5,65 +5,54 @@ import java.net.DatagramPacket;
 import java.net.SocketException;
 import java.io.IOException;
 
-public class CSReceiver implements Runnable
-{
+public class CSReceiver implements Runnable {
 	private Host hosts;
 	private DatagramSocket socket;
-	
-	public CSReceiver(Host hosts)
-	{
+
+	public CSReceiver(Host hosts) {
 		this.hosts = hosts;
 		socket = hosts.getSocket();
 	}
-	
-	public void run()
-	{
-		try
-		{
+
+	public void run() {
+		try {
 			byte[] incomingData;
 			int targetIndex;
-			
-			while (true)
-			{
+
+			while (true) {
 				// receive host info
 				incomingData = new byte[1024];
 				DatagramPacket incomingPacket = new DatagramPacket(incomingData, incomingData.length);
 				socket.receive(incomingPacket);
-				
+
 				// update the host info
 				String messageReceived = new String(incomingPacket.getData());
-				
+
 				String[] infoList = messageReceived.split(" ");
-				
+
 				System.out.println("receiver: IP: " + infoList[0] + " isServer: \"" + infoList[1] + "\"");
-				// array of info of the host received, at index 0 is the ip, at index 1 is the server status of that host
-				
-				targetIndex = hosts.searchHostbyIP(infoList[0]);
-				hosts.getHostInfo(targetIndex).updateTimeStamp(System.currentTimeMillis()); // update host's time stamp
-				hosts.getHostInfo(targetIndex).updateStatus(true); // update active status
-				
-				// get the sender's IP address
-//				String senderIP = incomingPacket.getAddress().toString().substring(1);
-				
-				// update the server status of the receiving packages
-				if(infoList[1].startsWith("true"))
-				{
-					hosts.getHostInfo(hosts.searchHostbyIP(infoList[0])).updateServerStatus(true);
-					System.out.println("receiver: update " + infoList[0] + " to be server");
-				}
-				else
-				{
-					hosts.getHostInfo(hosts.searchHostbyIP(infoList[0])).updateServerStatus(false);
-					System.out.println("receiver: update " + infoList[0] + " to be client");
+				// array of info of the host received, at index 0 is the ip, at index 1 is the
+				// server status of that host
+
+				try {
+					targetIndex = hosts.searchHostbyIP(infoList[0]);
+					hosts.getHostInfo(targetIndex).updateTimeStamp(System.currentTimeMillis()); // update host's time stamp
+					hosts.getHostInfo(targetIndex).updateStatus(true); // update active status
+					// update the server status of the receiving packages
+					if (infoList[1].startsWith("true")) {
+						hosts.getHostInfo(hosts.searchHostbyIP(infoList[0])).updateServerStatus(true);
+						System.out.println("receiver: update " + infoList[0] + " to be server");
+					} else {
+						hosts.getHostInfo(hosts.searchHostbyIP(infoList[0])).updateServerStatus(false);
+						System.out.println("receiver: update " + infoList[0] + " to be client");
+					}
+				} catch (IndexOutOfBoundsException e) {
+					System.out.println("Data was corrupt, sender will resend in a moment..");
 				}
 			}
-		}
-		catch (SocketException e)
-		{
+		} catch (SocketException e) {
 			e.printStackTrace();
-		}
-		catch (IOException e)
-		{
+		} catch (IOException e) {
 			e.printStackTrace();
 		}
 	}
